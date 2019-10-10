affaf2e43f5213b44ca8e6103e7a1f73ca94e95f
dan <120657866@qq.com>
2019-10-10 20:39:26 +0800

    添加拦截器

---
+++ src/index.ts
@@ -0,0 +1,40 @@
+import {AxiosRequestConfig,AxiosPromise, AxiosResponse} from './types/index'
+import xhr from './core/xhr'
+import {buildURL} from './helpers/url'
+import {transformRequest, transformResponse} from './helpers/data'
+import {precessHeaders} from './helpers/headers'
+
+function axios(config: AxiosRequestConfig): AxiosPromise {
+    processConfig(config)
+    return xhr(config).then(res => {
+        return transformResponseData(res)
+    })
+}
+
+
+function processConfig(config: AxiosRequestConfig): void {
+    config.headers = transformHeaders(config)
+    config.url = transformURL(config)
+    config.data = transformRequestData(config)
+}
+
+function transformURL(config: AxiosRequestConfig): string{
+    const {url, params} = config
+    return buildURL(url,params)
+}
+
+function  transformRequestData(config: AxiosRequestConfig): any {
+    return transformRequest(config.data)
+}
+
+function transformHeaders(config:AxiosRequestConfig): any {
+    const {headers = {},data} = config
+    return precessHeaders(headers,data)
+}
+
+function transformResponseData(res:AxiosResponse): AxiosResponse {
+    res.data = transformResponse(res.data)
+    return res
+}
+
+export default axios
